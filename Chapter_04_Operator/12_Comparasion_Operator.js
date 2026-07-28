0984a5b1c6ab6380b045b68a36034ee86e8411b3
dikshita-vitkare <dikshita@vitkare.com>
2026-07-28 08:28:23 +0530

    Add chapters on identifiers, literals, operators, statements, and switch
    Co-authored-by: CommandCodeBot <noreply@commandcode.ai>

---
+++ Chapter_04_Operator/12_Comparasion_Operator.js
@@ -0,0 +1,29 @@
+// Comparison Operator (will always result in the boolean, true or false)
+
+// =, ==, ===
+
+
+// = -> Assignment operator
+// == -> losse comparsion 
+// === -> strict comparsion 
+
+// > , < , >= , <= 
+
+
+// console.log(3 > 4);
+// console.log(3 < 4);
+// console.log(4 >= 4); // (4 > 4 or 4 == 4, or gate)
+
+console.log(5 == "5"); // lose couple comparsion , value compare
+console.log(5 === "5"); // stict not allowed - value + datatype both
+
+// === Strict check we will check for both the datatype and value
+// == Lose check we will check either value or data type.
+
+console.log(3 == "4");
+// ! -> not char
+console.log(5 != "5"); // lose -> datatype or value
+console.log(5 !== "5"); // true
+// console.log(5 !=== "5"); This dosn't
+
+console.log(5 === 5);
---
+++ Chapter_04_Operator/19_Inc_Dec_Operator.js
@@ -0,0 +1,22 @@
+let a = 10;
+console.log(++a + a + a++);
+console.log(a);
+
+// let i = 1;
+// let result = i++ + ++i;
+// console.log(result, i);
+
+
+let b = 10;
+console.log(++b + ++b);
+console.log(b);
+
+let c = 34;
+let result = c++;
+console.log(result);
+console.log(c);
+
+let d = 10;
+let e = d--;
+console.log(e);
+console.log(d);
